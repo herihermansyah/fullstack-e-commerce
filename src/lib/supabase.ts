1e6bf05f4f252bb5ac7dc647861a68b6b10edf60
heri hermansyah <54703307+herihermansyah@users.noreply.github.com>
2026-01-16 00:53:37 +0700

    Remove duplicate variable declarations
    Removed duplicate declarations of supabaseUrl and supabaseKey.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,9 +3,6 @@ import {createClient} from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_BASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SECRET_KEY;
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_BASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SECRET_KEY;
-
 export const supabase = createClient(
   supabaseUrl ?? "https://your-project-id.supabase.co", 
   supabaseKey ?? "your-anon-key"
