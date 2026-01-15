4f524d0e1ea51bfd5efc0a2625affe923e5b819c
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-01-16 00:45:50 +0700

    update supabase

--- src/app/(auth)/profile/[id]/editinformation/page.tsx
+++ src/app/(auth)/profile/[id]/editinformation/page.tsx
@@ -2,6 +2,8 @@ import EditInformation from "@/components/profile/form/form-edit-information";
 import { getUserIdEdit } from "@/feature/user/action";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+
 async function page({params}: {params: Promise<{id: string}>}) {
   const {id} = await params;
   const result = await getUserIdEdit(id);
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,10 @@
 import {createClient} from "@supabase/supabase-js";
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_BASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SECRET_KEY;
+
+// Jangan gunakan '!' jika tidak yakin variabelnya ada saat build
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_BASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_SECRET_KEY!
+  supabaseUrl || "https://placeholder.supabase.co",
+  supabaseKey || "placeholder"
 );
